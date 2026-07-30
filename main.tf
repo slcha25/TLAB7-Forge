8ce05c6dd3323c02df858556c99a359aa8d47f7a
slcha25 <s.leng.chan@outlook.com>
2026-07-29 17:29:29 -0700

    Add explicit VPC for security group

--- main.tf
+++ main.tf
@@ -1,15 +1,24 @@
-provider "aws" {  
-  region = "us-east-1"  
-}  
-  
-resource "aws_security_group" "sabotaged_sg" {  
-  name        = "tlab7-exposed-sg"  
-  description = "A dangerously exposed security group"  
-  
-  ingress {  
-    from_port   = 22  
-    to_port     = 22  
-    protocol    = "tcp"  
-    cidr_blocks = ["74.64.35.195/32"] # SABOTAGE: SSH exposed to the world  
-  }  
-}  
+provider "aws" {
+  region = "us-east-1"
+}
+
+resource "aws_vpc" "main" {
+  cidr_block = "10.0.0.0/16"
+
+  tags = {
+    Name = "TLAB7-VPC"
+  }
+}
+
+resource "aws_security_group" "sabotaged_sg" {
+  name        = "tlab7-exposed-sg"
+  description = "A dangerously exposed security group"
+  vpc_id      = aws_vpc.main.id
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["74.64.35.195/32"] # SABOTAGE: SSH exposed to the world
+  }
+}
